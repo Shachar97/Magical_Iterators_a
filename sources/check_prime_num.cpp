93979d8e8e5c5994fdb1b70474af88db36c4ff68
Shachar97 <shachar.ketz@gmail.com>
2023-06-04 14:01:39 +0300

    algorithm of prime num

---
+++ sources/check_prime_num.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "prime.hpp"
+using namespace std;
+
+
+namespace ariel{
+    // return 1 if given unsign int is a prime number else return 0.
+    bool isPrime( int number) {
+        if (number < 2) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for ( int i = 3; (i * i) <= number; i += 2){
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+}
